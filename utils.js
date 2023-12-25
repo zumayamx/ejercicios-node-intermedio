0c8c2771c277cbe4d97ce759590a2872f7cfd9f3
zumayamx <josezumayamx@MacBook-Air-de-Jose-3.local>
2023-12-25 16:54:22 -0600

    separacion carpetas

--- utils.js
+++
@@ -1,4 +0,0 @@
-import { createRequire } from 'node:module'
-const require = createRequire(import.meta.url) /* meta.url es la url del archivo actual */
-
-export const readJSON = (path) => require(path) /* Esto es para poder importar archivos json */
