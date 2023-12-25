3b0c958192d7162c4ca38d2f4b0789f47a0a73a3
zumayamx <josezumayamx@MacBook-Air-de-Jose-3.local>
2023-12-25 17:40:33 -0600

    slq connection

--- clase-5/app.js
+++ clase-5/app.js
@@ -1,19 +1,17 @@
-import express, { json } from 'express' 
+import express, { json } from 'express' // require -> commonJS
 import { moviesRouter } from './routes/movies.js'
 import { corsMiddleware } from './middlewares/cors.js'
-//import movies from './movies.json' <-- Esto no es válido en ECMAScriptModules
- 
 
 const app = express()
 app.use(json())
-app.use(corsMiddleware()) /* Aqui podrias pasar acceptedOroigins, tambien funciona por default */
-app.disable('x-powered-by') 
+app.disable('x-powered-by')
+
+/* Tener en cuenta cors middleware */
 
 app.use('/movies', moviesRouter)
 
 const PORT = process.env.PORT ?? 3000
 
 app.listen(PORT, () => {
-    console.log(`Server listening on port http://localhost:${PORT}`)
+  console.log(`server listening on port http://localhost:${PORT}`)
 })
-
--- clase-5/controllers/movies.js
+++ clase-5/controllers/movies.js
@@ -1,4 +1,5 @@
-import { MovieModel } from '../models/local-file-system/movie.js'
+//import { MovieModel } from '../models/local-file-system/movie.js'
+import { MovieModel } from '../models/mysql/movie.js'
 import { validateMovie, validatePartialMovie } from '../schemas/movies.js'
 
 export class MovieController {
